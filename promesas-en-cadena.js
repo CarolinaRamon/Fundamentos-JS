4662614a7742c7af977b5a7bd0ed0ce75373c0a6
Carolina <carolinaelizabeth.ramon@gmail.com>
2020-11-02 20:27:43 -0300

    Promesas en cadena

--- promesas-en-cadena.js
+++ promesas-en-cadena.js
@@ -19,8 +19,6 @@ let salarios = [{
 
 
 let getEmpleado = (id) => {
-   //El resolve se va a llamar si la promesa es exitosa y el reject si no es exitosa.
-   //Los nombres resolve y reject son opcionales, pero es un estándar llamarlos de esta manera.
     return new Promise( (resolve, reject)=> {
 
         let empleadoDB = empleados.find(empleado => {
@@ -31,7 +29,7 @@ let getEmpleado = (id) => {
         if(!empleadoDB){
             reject(`No existe un empleado con el ID ${id}`)
         }else{
-            resolve(empleadoDB);//En el caso de resolve, solo puedo enviar un argumento.
+            resolve(empleadoDB);
         }
     });
 }
@@ -46,13 +44,7 @@ let getSalario = (empleado) => {
         
         if(!salarioDB){
             reject(`No se encontró un salario para el empleado ${empleado.nombre}`)
-            // reject(`No se encontró un salario para el empleado ${empleado.nombre}`)
-            // reject(`No se encontró un salario para el empleado ${empleado.nombre}`)
-            // reject(`No se encontró un salario para el empleado ${empleado.nombre}`)
-            // reject(`No se encontró un salario para el empleado ${empleado.nombre}`)
-            //Por más que me equivoque y repita el reject no se vuelve a disparar. Aunque el código sí se sigue ejecutando:
             console.log('Otra línea después.');
-            //Si fuera un callback sí se vuelve a disparar.
         }else{
             resolve({
                 nombre: empleado.nombre,
